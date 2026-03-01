9a91d2507723be57887b5e0187b29bb84d002df4
Swas-nayak <swasthisnayak@gmail.com>
2026-03-01 19:31:00 +0530

    Check if Array is Sorted by taking input

--- arrays/CheckSorted.java
+++ arrays/CheckSorted.java
@@ -14,8 +14,10 @@ public class CheckSorted {
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the size of the array:");
     int size = sc.nextInt();
     int[] arr = new int[size];
+    System.out.println("Enter the elements of the array:");
     for (int i = 0; i < size; i++) {
       arr[i] = sc.nextInt();
     }
